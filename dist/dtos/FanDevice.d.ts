ee71ef9d37fc078c92f56738134281942b24047d
Alex Banica <ionut.alexandru.banica@gmail.com>
2025-10-17 12:52:57 +0200

    dist

--- dist/dtos/FanDevice.d.ts
+++ dist/dtos/FanDevice.d.ts
@@ -1,14 +1,17 @@
 import { FanDeviceConfig } from './FanDeviceConfig.js';
 export declare class FanDevice {
-    on: boolean;
+    private _on;
     speed: number;
     rotation: boolean;
     config: FanDeviceConfig;
-    private readonly minSpeed;
-    private readonly maxSpeed;
+    private readonly minDeviceSpeed;
+    private readonly maxDeviceSpeed;
+    private readonly onDeviceSpeed;
     constructor(config: FanDeviceConfig);
     private speedToPercent;
     private percentToSpeed;
     getDeviceSpeed(): number;
     setDeviceSpeed(speed: number): void;
+    set on(on: boolean);
+    get on(): boolean;
 }
--- dist/dtos/FanDevice.js
+++ dist/dtos/FanDevice.js
@@ -1,14 +1,16 @@
 export class FanDevice {
-    on = false;
+    _on = false;
     speed = 0;
     rotation = false;
     config;
-    minSpeed;
-    maxSpeed;
+    minDeviceSpeed;
+    maxDeviceSpeed;
+    onDeviceSpeed;
     constructor(config) {
         this.config = config;
-        this.minSpeed = 0;
-        this.maxSpeed = 3;
+        this.minDeviceSpeed = 0;
+        this.onDeviceSpeed = 1;
+        this.maxDeviceSpeed = 3;
     }
     speedToPercent(s) {
         if (s === 0) {
@@ -38,8 +40,20 @@ export class FanDevice {
         return this.percentToSpeed(this.speed);
     }
     setDeviceSpeed(speed) {
-        speed = Math.min(this.maxSpeed, Math.max(this.minSpeed, Math.round(speed)));
+        speed = Math.min(this.maxDeviceSpeed, Math.max(this.minDeviceSpeed, Math.round(speed)));
         this.speed = this.speedToPercent(speed);
+    }
+    set on(on) {
+        this._on = on;
+        if (on) {
+            this.setDeviceSpeed(this.onDeviceSpeed);
+        }
+        else {
+            this.setDeviceSpeed(this.minDeviceSpeed);
+        }
+    }
+    get on() {
+        return this._on;
     }
 }
 //# sourceMappingURL=FanDevice.js.map
